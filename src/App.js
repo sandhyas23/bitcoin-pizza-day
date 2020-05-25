ea967f77f6e4a473bf207c60fcf0d616f1119695
Sandhya <ssankaran3@horizon.csueastbay.edu>
2020-05-24 18:09:43 -0700

    Designed initial page with countdown and contact

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
+import moment from 'moment';
+import pizza from './images/pizza.png';
+import message from './images/message.png'
+import Countdown from "./Countdown";
+import BitcoinPrice from "./BitcoinPrice";
+import Message from "./Message";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+
+    constructor(props) {
+
+        super(props);
+        this.state = { showModal:false}
+
+    }
+
+    handleCloseModal () {
+        this.setState({ showModal: false});
+    }
+
+  render() {
+      //console.log("in app", this.state.bpi["USD"]=== undefined);
+      const currentDate = new Date();
+      const year = (currentDate.getMonth() === 11 && currentDate.getDate() > 23) ? currentDate.getFullYear() + 1 : currentDate.getFullYear();
+    return (
+        <div className="App content-wrap">
+          <img className={"pizzaImage"} src={pizza} />
+            <Countdown date={`${year+1}-05-22T00:00:00`} />
+            <img className={"message"} src={message} onClick={()=>this.setState({showModal:!this.state.showModal})}/>
+            <BitcoinPrice />
+            {this.state.showModal ? <Message showModal={this.state.showModal} closeModal={()=>this.handleCloseModal()}/> :null}
+            <footer>
+                Copyright © 2020 Bitcoin Pizza Day - All Rights Reserved.<br/>
+                    Powered by <a href={"https://www.coindesk.com/price/bitcoin"} >CoinDesk</a>
+            </footer>
+        </div>
+    );
+  }
+
+
 }
 
-export default App;
---
+++ src/BitcoinPrice.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+export default class BitcoinPrice extends React.Component {
+
+    constructor(props) {
+
+        super(props);
+        this.state = {bpi:{},  currency:'USD'}
+
+    }
+    componentWillMount() {
+        console.log("component app mounted");
+        fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+            .then(res=>res.json())
+            .then(data =>
+                this.setState({bpi:data.bpi}))
+
+    }
+
+    handleSelect(e){
+        this.setState({currency:e.target.value})
+    }
+
+
+    render() {
+        const currencySymbols = {'USD':'$', 'EUR':'€' ,'GBP':'£'}
+
+
+        return(
+            <div className={"Countdown"}>
+                <div className="Countdown-col currency">
+                    <span> 1 bitcoin = </span>
+                </div>
+                <div className="Countdown-col currency">
+                <select placeholder='Select your currency' onChange={(e)=>this.handleSelect(e)}
+                        name="currency" value={this.state.currency}>
+                    {
+                        Object.keys(this.state.bpi).map((element,index,array)=>{
+                            return <option key={`currency${element}${index}`}>
+                                    {element}
+                            </option>
+                        })
+                    }
+                </select>
+                </div >
+                {this.state.bpi[this.state.currency] !== undefined ?
+                    <div className="Countdown-col currency">
+                        <span>
+                            {currencySymbols[this.state.bpi[this.state.currency].code] !== undefined ?
+                                currencySymbols[this.state.bpi[this.state.currency].code]
+                            :
+                            null}
+                        </span>
+                        <span>
+                            {this.state.bpi[this.state.currency].rate }
+                        </span>
+                        </div>
+                    :
+                    null
+                    }
+
+
+                    {this.state.bpi[this.state.currency] !== undefined ?
+                        <div className="currency"> He had 2 full 🍕 worth <mark>$ {parseFloat(this.state.bpi["USD"].rate) * 10000}</mark> on this day 😲
+                        </div>
+                        :
+                        null
+                    }
+
+
+
+            </div>
+        );
+    }
+}
